b85e6bc27eb1ba2a29c849904f80e224f974a3d9
abdullah <abdullahaq4838@gmail.com>
2025-01-22 14:44:22 +0300

    Update

--- src/Main.kt
+++ src/Main.kt
@@ -1,38 +1,50 @@
 import java.time.LocalTime
 import java.time.Duration
 
-data class User (val name: String = "aa",
+data class User (var id: Int = 0,
                  val type: String,
-                 val date: LocalTime = LocalTime.now().withNano(0),
-                 var ip: String = "")
+                 val date: LocalTime = LocalTime.now(),
+                 var ip: String = ""){
 
-fun assignIp(list: MutableList<User>, user: User, ip: String) {
-    var assigned = false
-    for ((index, preUser) in list.withIndex()) {
-        if (Duration.between(preUser.date, LocalTime.now()).toSeconds() > 10) {
-            list[index] = user
-            user.ip = ip + index
-            assigned = true
-            break
+    companion object{
+        var idCounter = 0
+        val LIFE_TIME_SECONDS = 10
+    }
+
+    init {
+        id = ++idCounter
+    }
+
+    fun assignIP(list: MutableList<User>, ipPrefix: String) {
+        var assigned = false
+        for ((index, user) in list.withIndex()) {
+            if (Duration.between(user.date, LocalTime.now()).toSeconds() > LIFE_TIME_SECONDS) {
+                list[index] = this
+                this.ip = "$ipPrefix.$index"
+                assigned = true
+                break
+            }
         }
+        if (!assigned) {
+            list.add(this)
+            this.ip = "$ipPrefix.${list.size - 1}"
+        }
+        println(list.toString())
     }
-    if (!assigned) {
-        list.add(user)
-        user.ip = ip + (list.size - 1)
-    }
-    println(list.toString())
 }
 
 fun main() {
     val clientIps = mutableListOf<User>()
+    val clientPrefix = "192.0.0"
     val serverIps = mutableListOf<User>()
+    val serverPrefix = "127.0.0"
     while (true) {
         print("Enter your type: ")
         val type = readln()
         val user = User(type = type)
         when (user.type) {
-            "c" -> assignIp(clientIps, user, "192.0.0.")
-            "s" -> assignIp(serverIps, user, "127.0.0.")
+            "c" -> user.assignIP(clientIps, clientPrefix)
+            "s" -> user.assignIP(serverIps, serverPrefix)
         }
     }
 }
