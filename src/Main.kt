ac51de9c06227dc2cf248143393fceb7af5a0863
abdullah <abdullahaq4838@gmail.com>
2025-01-22 22:58:42 +0300

    Update

--- src/Main.kt
+++ src/Main.kt
@@ -5,46 +5,47 @@ data class User (var id: Int = 0,
                  val type: String,
                  val date: LocalTime = LocalTime.now(),
                  var ip: String = ""){
-
     companion object{
         var idCounter = 0
         val LIFE_TIME_SECONDS = 10
     }
-
     init {
         id = ++idCounter
     }
+}
 
-    fun assignIP(list: MutableList<User>, ipPrefix: String) {
+class Users(val clientList: MutableList<User> = mutableListOf(),
+            val serverList: MutableList<User> = mutableListOf(),
+            val clientPrefix: String = "192.0.0",
+            val serverPrefix: String = "127.0.0") {
+    fun assignIP(user: User) {
+        val (list, ipPrefix) = if (user.type == "client") Pair(clientList, clientPrefix) else Pair(serverList, serverPrefix)
         var assigned = false
-        for ((index, user) in list.withIndex()) {
-            if (Duration.between(user.date, LocalTime.now()).toSeconds() > LIFE_TIME_SECONDS) {
-                list[index] = this
-                this.ip = "$ipPrefix.$index"
+        for ((index, oldUser) in list.withIndex()) {
+            if (Duration.between(oldUser.date, LocalTime.now()).toSeconds() > User.LIFE_TIME_SECONDS) {
+                list[index] = user
+                user.ip = "$ipPrefix.$index"
                 assigned = true
                 break
             }
         }
         if (!assigned) {
-            list.add(this)
-            this.ip = "$ipPrefix.${list.size - 1}"
+            list.add(user)
+            user.ip = "$ipPrefix.${list.size - 1}"
         }
         println(list.toString())
     }
 }
 
 fun main() {
-    val clientIps = mutableListOf<User>()
-    val clientPrefix = "192.0.0"
-    val serverIps = mutableListOf<User>()
-    val serverPrefix = "127.0.0"
+    val users = Users()
     while (true) {
         print("Enter your type: ")
         val type = readln()
         val user = User(type = type)
         when (user.type) {
-            "c" -> user.assignIP(clientIps, clientPrefix)
-            "s" -> user.assignIP(serverIps, serverPrefix)
+            "client", "server" -> users.assignIP(user)
+            else -> println("Invalid input")
         }
     }
 }
